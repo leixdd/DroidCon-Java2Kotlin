b853daf598ff741df088f12cb571a6d2b9bacc6a
lei <lecias.ariel@gmail.com>
2018-10-20 11:30:54 +0800

    AC4

--- app/src/main/java/com/example/lei/droidconkot/MainActivity.kt
+++ app/src/main/java/com/example/lei/droidconkot/MainActivity.kt
@@ -6,26 +6,25 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    var CarList : ArrayList<K_Car> = ArrayList<K_Car>()
+    var TruckList : ArrayList<K_Car> = ArrayList<K_Car>()
+    var numLanes : Int = 0
+    var road : String = ""
+
+    lateinit var secondRoad : String
+
+    //Exec4
+    //var Names : ArrayList<String> = ArrayList<String>();
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //basketPlayers = arrayListOf<String>("Josh", "Lei", "Kath")
-        //elvis operator = <?>  if null don't crash :V [hahahahaha]
-        plate_number.setText(basketPlayers?.get(0))
+
+        CarList = arrayListOf(K_Car("ASC123", "McLaren", false), K_Car("AS2123", "Veyron", false))
+        secondRoad = "Pangalawang Road"
     }
 
-    //Must have default value since it was considered as primitive (SABI NI MAM)
-    var homeCourt : Boolean = false
-
-    //lateinit  -> will create or set default value when onCreate()
-    lateinit var basketballTeam : String
-    lateinit var color1: String
-    lateinit var color2: String
-
-    //Is Nullable
-    var basketPlayers : ArrayList<String>? = null
 
 }
